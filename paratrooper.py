a722668a41a72d218ee29659ff7c4fd63e185001
Arun M <arunm@Aruns-MacBook-Air.local>
2020-08-28 01:36:37 +0530

    refactored code for checking keyboard events and updating the screen in paratrooper run_game method  into helper methods, _check_events() and _update_events()

--- paratrooper.py
+++ paratrooper.py
@@ -24,14 +24,22 @@ class Paratrooper:
         """Start the main loop for the game """
         while True:
             # Watch for keyboard and mouse events
-            for event in pygame.event.get():
+            self._check_events()
+            self._update_events()
+
+            
+    
+    def _check_events(self):
+        for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+    
+    def _update_events(self):
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+        
 if __name__ == '__main__':
     # Make a game instance, and run the game.
     para = Paratrooper()
